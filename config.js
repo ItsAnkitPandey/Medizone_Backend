e5cc916462db1946dc7086c29d7406abfaeea79d
Ankit Pandey <112678495+ItsAnkitPandey@users.noreply.github.com>
2026-01-20 23:48:21 +0530

    Enforce use of environment variables for MongoDbUrl and JWT_SECRET
    Removed default values for MongoDbUrl and JWT_SECRET to enforce the use of environment variables.

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 // To use: npm install dotenv, then add: import dotenv from 'dotenv'; dotenv.config();
 
 export const PORT = process.env.PORT || 5555;
-export const MongoDbUrl = process.env.MONGODB_URI || 'mongodb+srv://medizoneadmin:Medi%40123@medizone.nago4u4.mongodb.net/Medizone?retryWrites=true&w=majority';
+export const MongoDbUrl = process.env.MONGODB_URI;
 
 // It's highly recommended to use environment variables for secrets.
-export const JWT_SECRET = process.env.JWT_SECRET || "medi$zone";
+export const JWT_SECRET = process.env.JWT_SECRET ;
 
 // Email configuration
 export const EMAIL_USER = process.env.EMAIL_USER;
